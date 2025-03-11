48e90f3f8d79389eb11aa2fb30effdc500a92fd6
healthp <healthp@proton.me>
2025-03-11 12:23:31 +0800

    Delete version-final directory

--- version-final/jdbc-student-tracker-final/sql-scripts/01-create-user.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER 'demo'@'localhost' IDENTIFIED BY 'demo';
-
-GRANT ALL PRIVILEGES ON * . * TO 'demo'@'localhost';
-
-alter user 'demo'@'localhost' identified with mysql_native_password by 'demo';
